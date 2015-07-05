ad7ccf7ba1093587ca62a9330898f6df2fdb5c64
basilp78@gmail.com <root@localhost>
2015-07-05 14:01:39 +0000

    New files

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["basil1"] = { "site_title" => "Basils website coming soon", "port" => 80, "domain" => "basil1.mylabserver.com" }
+default["apache"]["sites"]["basil2"] = { "site_title" => "Basils website coming soon", "port" => 80, "domain" => "basil1b.mylabserver.com" } 
