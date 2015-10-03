f21219e7224e8cc21436e64eee673c3e34e43c20
basilp78@gmail.com <basilp78@gmail.com>
2015-10-03 21:30:08 +0000

    Updated role to include java

--- environments/dev.rb
+++
@@ -1,9 +0,0 @@
-name "dev"
-description "This is development environment"
-cookbook "apache", "= 0.1.5"
-cookbookk "elasticsearch", "=0.3.14" 
-override_attributes({
-	"author" => {
-		"name" => "my new author name"
-	}
-})
--- roles/elasticsearch.rb
+++ roles/elasticsearch.rb
@@ -1,3 +1,3 @@
 name "elasticsearch"
 description "This is the role for nodes joining elasticsearch"
-run_list "recipe[elasticsearch]", "recipe[elasticsearch::aws]", "recipe[elasticsearch::monit]", "recipe[elasticsearch::data]", "recipe[elasticsearch::ebs]", "recipe[elasticsearch::proxy]", "recipe[elasticsearch::search_discovery]"
+run_list "recipe[java]", "recipe[elasticsearch]", "recipe[elasticsearch::aws]", "recipe[elasticsearch::monit]", "recipe[elasticsearch::data]", "recipe[elasticsearch::ebs]", "recipe[elasticsearch::proxy]", "recipe[elasticsearch::search_discovery]"
